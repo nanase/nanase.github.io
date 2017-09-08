8b3728d04a2c187c8600c7d0ebb622a833c0987a
Tomona Nanase <nanase@users.noreply.github.com>
2017-09-08 16:55:55 +0900

    よく使うクラスを作成、リファクタリング

--- kemofure/status/js/kemofure.js
+++ kemofure/status/js/kemofure.js
@@ -1,6 +1,50 @@
-const json_path = 'https://nanase.onl/kemofure/stat/';
-const diff_path = 'https://nanase.onl/kemofure/diff/';
+const current_path = 'https://nanase.onl/kemofure/stat/';
 const speed_path = 'https://nanase.onl/kemofure/speed/';
+const diff_path = 'https://nanase.onl/kemofure/diff/';
+
+class TimeoutUpdater {
+  constructor(callback, interval) {
+    this.callback = callback;
+    this.interval = interval;
+    this.timeout_id = null;
+  }
+
+  start() {
+    this.stop();
+    this.timeout_id = setTimeout(this.callback, this.interval);
+  }
+
+  stop() {
+    if (this.timeout_id)
+      clearInterval(this.timeout_id);
+  }
+}
+
+class Storager {
+  constructor(storage_name, default_value = null) {
+    this.storage_name = storage_name;
+    this.default_value = default_value;
+    this.get();
+  }
+
+  get() {
+    const value = localStorage.getItem(this.storage_name)
+
+    if (value == null || typeof value === 'undefined') {
+      localStorage.setItem(this.storage_name, this.default_value);
+      return this.default_value;
+    }
+
+    return value;
+  }
+
+  setIfNotNull(value) {
+    if (value != null && typeof value !== 'undefined')
+      localStorage.setItem(this.storage_name, value);
+
+    return this.get();
+  }
+}
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -10,8 +54,12 @@ function diffText(value) {
   return value >= 0 ? '+ ' + numberWithCommas(value) : '- ' + numberWithCommas(-value);
 }
 
-function load_json() {
-  $.getJSON(json_path, function (json) {
+// -------------------------
+let old_json = { time: 0, play_count: 0, comment_count: 0, mylist_count: 0 };
+let currentTimeout = new TimeoutUpdater(load_current, 5000);
+
+function load_current() {
+  $.getJSON(current_path, function (json) {
     if (old_json.play_count != json.play_count) {
       $('.obox-view .display-big').text(numberWithCommas(json.play_count));
       $('.obox-view').removeClass('updated');
@@ -47,69 +95,57 @@ function load_json() {
     }
 
     old_json = json;
-    setTimeout(load_json, 5000);
+    currentTimeout.start();
   }).fail(function () {
-    setTimeout(load_json, 5000);
+    currentTimeout.start();
   });
 }
 
-// -------------------------
-let speedDuration;
-let speedUpdateTimeout = null;
+function initialize_current() {
+  //currentTimeout 
+  load_current();
+}
 
-if (!(speedDuration = localStorage.getItem('kfm_speedDuration')))
-  speedDuration = 'h';
+// -------------------------
+const speedDuration = new Storager('kfm_speedDuration', 'h');
+let speedTimeout;
 
 function toggle_speed() {
-  if (speedDuration == 'h')
-    speedDuration = 'm';
+  speedTimeout.stop();
+
+  if (speedDuration.get() == 'h')
+    load_speed('m');
   else
-    speedDuration = 'h';
-
-  stopSpeedUpdate();
-  load_speed(speedDuration);
+    load_speed('h');
 }
 
-function startSpeedUpdate() {
-  stopSpeedUpdate();
-  speedUpdateTimeout = setTimeout(load_speed, 60000);
-}
+function load_speed(duration) {
+  duration = speedDuration.setIfNotNull(duration);
 
-function stopSpeedUpdate() {
-  if (speedUpdateTimeout)
-    clearInterval(speedUpdateTimeout);
-}
-
-function load_speed(duration = 'h') {
-  if (duration != null)
-    localStorage.setItem('kfm_speedDuration', (speedDuration = duration));
-
-  $.get(speed_path, { duration: speedDuration },
+  $.get(speed_path, { duration },
     function (json) {
       $('.diff-view').text(diffText(json.play_count));
       $('.diff-comment').text(diffText(json.comment_count));
       $('.diff-mylist').text(diffText(json.mylist_count));
-      $('.diff-view ~ .display-name').text('views/' + speedDuration);
-      $('.diff-comment ~ .display-name').text('comments/' + speedDuration);
-      $('.diff-mylist ~ .display-name').text('mylists/' + speedDuration);
-      startSpeedUpdate()
+      $('.diff-view ~ .display-name').text('views/' + duration);
+      $('.diff-comment ~ .display-name').text('comments/' + duration);
+      $('.diff-mylist ~ .display-name').text('mylists/' + duration);
+      speedTimeout.start();
     }, 'json').fail(function () {
-      startSpeedUpdate()
+      speedTimeout.start();
     });
 }
 
+function initialize_speed() {
+  speedTimeout = new TimeoutUpdater(load_speed, 60000);
+  load_speed();
+}
+
 // -------------------------
-
 let chart;
-let old_json = { time: 0, play_count: 0, comment_count: 0, mylist_count: 0 };
-let graphDuration, graphKind;
-let graphUpdateTimeout = null;
-
-if (!(graphKind = localStorage.getItem('kfm_graphKind')))
-  graphKind = 'd';
-
-if (!(graphDuration = localStorage.getItem('kfm_graphDuration')))
-  graphDuration = '1';
+const graphDuration = new Storager('kfm_graphDuration', '1');
+const graphKind = new Storager('kfm_graphKind', 'd');
+let graphTimeout;
 
 function convertIntervalNumber(interval) {
   switch (interval) {
@@ -120,16 +156,6 @@ function convertIntervalNumber(interval) {
     case 86400: return 'per 1 day';
     default: 'per ' + interval + ' seconds';
   }
-}
-
-function startGraphUpdate() {
-  stopGraphUpdate();
-  graphUpdateTimeout = setTimeout(load_graph, 60000);
-}
-
-function stopGraphUpdate() {
-  if (graphUpdateTimeout)
-    clearTimeout(graphUpdateTimeout);
 }
 
 function initialize_graph() {
@@ -162,9 +188,9 @@ function initialize_graph() {
       events: {
         selection: function (event) {
           if (event.resetSelection)
-            startGraphUpdate();
+            graphTimeout.start();
           else
-            stopGraphUpdate();
+            graphTimeout.stop();
         }
       }
     },
@@ -235,27 +261,23 @@ function initialize_graph() {
       name: 'Mylist',
       color: '#ec7835',
       lineWidth: 1,
-    }
-    ]
+    }]
   });
+
+  graphTimeout = new TimeoutUpdater(load_graph, 60000);
+  load_graph();
 }
 
 function load_graph(kind = null, duration = null) {
-  if (kind != null)
-    localStorage.setItem('kfm_graphKind', (graphKind = kind));
-
-  if (duration != null)
-    localStorage.setItem('kfm_graphDuration', (graphDuration = duration));
+  kind = graphKind.setIfNotNull(kind);
+  duration = graphDuration.setIfNotNull(duration);
 
   $('.obox.obox-mini').removeClass('obox-selected');
-  $('.obox-graph-selector-' + graphKind + ', .obox-graph-selector-' + graphDuration).addClass('obox-selected');
+  $('.obox-graph-selector-' + kind + ', .obox-graph-selector-' + duration).addClass('obox-selected');
 
-  $.get(diff_path, {
-    duration: graphDuration,
-    kind: graphKind
-  },
+  $.get(diff_path, { duration, kind },
     function (json) {
-      let convert = (data, series) => data.time.map((t, i) => [t * 1000, series[i]]);
+      const convert = (data, series) => data.time.map((t, i) => [t * 1000, series[i]]);
       chart.series[0].setData(convert(json, json.play_count));
       chart.series[1].setData(convert(json, json.comment_count));
       chart.series[2].setData(convert(json, json.mylist_count));
@@ -263,12 +285,11 @@ function load_graph(kind = null, duration = null) {
       chart.redraw();
     }, 'json');
 
-  startGraphUpdate();
+  graphTimeout.start();
 }
 
 $(() => {
-  load_json();
-  load_speed();
+  initialize_current();
+  initialize_speed();
   initialize_graph();
-  load_graph();
 });
